06ca5693eee627978f56537050b23bd7e8168cfc
Sebastian_Saavedra <sebastiansaavedr@gmail.com>
2020-10-14 22:35:47 -0300

    Primer Commit

---
+++ Assets/02_Scripts/TEST/TEST_Interact.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_Interact : MonoBehaviour
+{
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Entro al area");
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Agarro la pocion");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/02_Scripts/TEST/TEST_Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_Movement : MonoBehaviour
+{
+    //Editables
+    [SerializeField] float movSpeed;
+    [SerializeField] float runSpeed;
+
+    //Privadas
+    Rigidbody2D rb2d;
+    Vector2 objVelocity;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        objVelocity = moveInput.normalized * movSpeed;
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            movSpeed += runSpeed;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            movSpeed -= runSpeed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + objVelocity * Time.fixedDeltaTime);
+    }
+}
