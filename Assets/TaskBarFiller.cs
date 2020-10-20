06d4b06a65563bdb4627e1cbed7c23aa4c593ba6
Jose Varela <varelacen@hotmail.com>
2020-10-20 19:10:09 -0300

    Bruh

---
+++ Assets/TaskBarFiller.cs
@@ -0,0 +1,16 @@
+﻿using Com.MaluCompany.TestGame;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskBarFiller : MonoBehaviour
+{
+
+    [SerializeField] GameManager taskFiller;
+    [SerializeField] Slider taskBar;
+public void OnTaskComplete() 
+    {
+        taskBar.value = taskBar.value + 1;
+    }
+}
