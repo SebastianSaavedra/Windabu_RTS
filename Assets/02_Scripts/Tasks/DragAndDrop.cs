49981db5f2337074b3771b1a444dfd2a523582e6
Carlos Michea A <carlos.15.michea@gmail.com>
2020-10-20 19:13:35 -0300

    Drag n drop creado
    se divide en 2 scripts, DragAndDrop y ItemSlot

---
+++ Assets/02_Scripts/Tasks/DragAndDrop.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDrop : MonoBehaviour, IDragHandler, IPointerDownHandler, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField] private Canvas canvas;
+
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    //Click
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Clicked");
+    }
+
+    //1st frame of drag
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    //Every frame dragged
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Move like cursor
+        rectTransform.anchoredPosition += eventData.delta;
+    }
+
+    //Unclick
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        canvasGroup.blocksRaycasts = true;
+    }
+}
