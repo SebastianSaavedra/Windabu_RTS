c99952728139c9670189056b42d9790ff6624160
Sebastian_Saavedra <sebastiansaavedr@gmail.com>
2020-10-20 02:28:42 -0300

    Test_RandomImage

---
+++ Assets/02_Scripts/TEST/Test_ImageRandomPos.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_ImageRandomPos : MonoBehaviour
+{
+    [SerializeField] List<GameObject> images = new List<GameObject>();
+
+    void Start()
+    {
+        //GameObject randImage = images[Random.Range(0,images.Count)];
+        foreach (GameObject imagen in images)
+        {
+            imagen.transform.localPosition = new Vector2((Random.Range(-212f, 212f)), Random.Range(-363f, 363f));
+        }
+        //randImage.transform.localPosition = new Vector2((Random.Range(0,100f)),Random.Range(0,100f));
+        //Debug.Log(randImage.transform.localPosition);
+    }
+}
