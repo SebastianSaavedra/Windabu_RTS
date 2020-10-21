a852f1d86f633fa26aef61f20c15f96bc582549a
Jose Varela <varelacen@hotmail.com>
2020-10-20 22:30:27 -0300

    Maybe

---
+++ Assets/PlayerNaming.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Com.MaluCompany.TestGame;
+
+public class PlayerNaming : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI playerName;
+    private TEST_Movement target;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetTarget(TEST_Movement _target)
+    {
+        if (_target == null)
+        {
+            return;
+        }
+        target = _target;
+        if (playerName != null)
+        {
+            playerName.text = target.photonView.Owner.NickName;
+        }
+    }
+}
