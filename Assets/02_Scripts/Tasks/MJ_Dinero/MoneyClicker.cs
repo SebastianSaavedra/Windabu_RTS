921c3f28f0acda76cce60ca208459f080507a1b6
Carlos Michea A <carlos.15.michea@gmail.com>
2020-10-20 23:20:22 -0300

    Minijuego dinero

---
+++ Assets/02_Scripts/Tasks/MJ_Dinero/MoneyClicker.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyClicker : MonoBehaviour
+{
+    public GameObject shareButton, sharePanel;
+
+    int likes;
+    public bool isPublic;
+
+    private void Start()
+    {
+
+    }
+
+    public void Like()
+    {
+        //Limit
+        if(likes <= 99)
+        {
+            likes += 1;
+        }
+        else { Debug.Log("Full Money"); }
+
+        if(likes <= 0)
+        {
+            shareButton.SetActive(true);
+        }
+    }
+
+    public void Share()
+    {
+        sharePanel.SetActive(true);
+    }
+
+    public void ChangePublic()
+    {
+        if (isPublic)
+        {
+            isPublic = false;
+        }
+        else
+        {
+            isPublic = true;
+        }
+    }
+
+    public void Confirm()
+    {
+        Debug.Log("$$$");
+
+        // Minijuegos.m_clicks(likes);
+
+        int macroLike = 0;
+        while(macroLike <= likes - 10)
+        {
+            macroLike += 10;
+        }
+
+        Minijuegos.m_clicks(macroLike / 10);
+
+        //Reset
+        likes -= macroLike;
+        isPublic = false;
+        sharePanel.SetActive(false);
+        shareButton.SetActive(false);
+    }
+}
