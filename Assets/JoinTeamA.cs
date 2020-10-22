e6e6652a2d1cbd39620876c58e7b41193103baf6
Jose Varela <varelacen@hotmail.com>
2020-10-21 23:53:00 -0300

    Teams

---
+++ Assets/JoinTeamA.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinTeamA : MonoBehaviourPunCallbacks
+{
+    public void JoinTeam() 
+{
+        Debug.Log("TeamA");
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            Debug.Log(player.name);
+            if (player.GetComponentInParent<PlayerId>().id == PhotonNetwork.LocalPlayer.ActorNumber)
+            {
+                player.GetComponentInParent<PlayerTeam>().TeamA = true;
+            }
+        }
+    }
+
+}
---
+++ Assets/JoinTeamB.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoinTeamB : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    public void JoinTeam()
+    {
+        Debug.Log("TeamB");
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            Debug.Log(player.name);
+            if (player.GetComponentInParent<PlayerId>().id == PhotonNetwork.LocalPlayer.ActorNumber)
+            {
+                player.GetComponentInParent<PlayerTeam>().TeamB = true;
+            }
+        }
+    }
+}
