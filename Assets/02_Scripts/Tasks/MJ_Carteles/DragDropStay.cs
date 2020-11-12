2a0e4a531eacca86cc413630dff2c74e3d453741
Carlos Michea A <carlos.15.michea@gmail.com>
2020-11-12 15:39:13 -0300

    Prefab Impresora - DragDropStay

---
+++ Assets/02_Scripts/Tasks/MJ_Carteles/DragDropStay.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDropStay : MonoBehaviour, IDragHandler, IPointerDownHandler, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField] private Canvas canvas;
+    public float itemDragId;
+
+    [HideInInspector] public Vector3 posInicial;
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+
+    public bool locked = false;
+
+    private void Awake()
+    {
+        canvas = GetComponentInParent<Canvas>();
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        posInicial = rectTransform.localPosition;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = .6f;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Move like cursor
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f;
+        if (!locked)
+        {
+            canvasGroup.blocksRaycasts = true;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Clicked");
+    }
+
+    private void OnDisable()
+    {
+        rectTransform.localPosition = posInicial;
+    }
+}
--- Assets/02_Scripts/Tasks/MJ_Carteles/ItemSlot.cs
+++ Assets/02_Scripts/Tasks/MJ_Carteles/ItemSlot.cs
@@ -5,12 +5,32 @@ using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour, IDropHandler
 {
+    public float slotId;
+    private float draggedId;
+
     public void OnDrop(PointerEventData eventData)
     {
+        Debug.Log("drop");
+
         //Snap into position
-        if(eventData.pointerDrag != null)
+        if (eventData.pointerDrag != null)
         {
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            // Scan Id
+            draggedId = eventData.pointerDrag.GetComponent<DragDropStay>().itemDragId;
+            if(draggedId == slotId)
+            {
+                // Move to slot
+                eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+                // Lock
+                eventData.pointerDrag.GetComponent<DragDropStay>().locked = true;
+                //Disable
+                gameObject.SetActive(false);
+            }
+            // Failed Scan
+            else
+            {
+                Debug.Log("Aqui no es bro");
+            }
         }
     }
 }
