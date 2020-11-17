c9535058c4db8385b206e30c022fd78275668fc8
Jose Varela <varelacen@hotmail.com>
2020-11-17 11:54:03 -0300

    Commit Logica Prod Queue

--- Assets/02_Scripts/Game/UIManager.cs
+++ Assets/02_Scripts/Game/UIManager.cs
@@ -7,8 +7,10 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI moneyText;
+    [SerializeField] TextMeshProUGUI moneyText2;
     void Update()
     {
         moneyText.text = MinigameManager.dinero.ToString();
+        moneyText2.text = MinigameManager.dinero.ToString();
     }
 }
