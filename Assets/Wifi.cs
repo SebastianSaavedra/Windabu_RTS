8b5fda922c5bcec8a25771bfc61c41a11a46c87b
Sebastian_Saavedra <sebastiansaavedr@gmail.com>
2020-11-19 08:03:41 -0300

    wifi

--- Assets/Wifi.cs
+++ Assets/Wifi.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Wifi : MonoBehaviour
 {
-    [SerializeField] GameObject wifi, wifint;
+    [SerializeField] GameObject celu,wifi, wifint;
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                celu.SetActive(true);
+            }
             wifint.SetActive(false);
             wifi.SetActive(true);
         }
