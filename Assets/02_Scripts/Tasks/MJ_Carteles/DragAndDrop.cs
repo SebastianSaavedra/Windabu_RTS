0b848a58998cb6433628cd2fc2cf6c996e7e6570
Sebastian_Saavedra <sebastiansaavedr@gmail.com>
2020-11-22 03:22:46 -0300

    Minijuego Cartel Terminado

--- Assets/02_Scripts/Tasks/MJ_Carteles/DragAndDrop.cs
+++ Assets/02_Scripts/Tasks/MJ_Carteles/DragAndDrop.cs
@@ -36,7 +36,7 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IPointerDownHandler, IBe
     //1st frame of drag
     public void OnBeginDrag(PointerEventData eventData)
     {
-        canvasGroup.alpha = .6f;
+        canvasGroup.alpha = .85f;
         canvasGroup.blocksRaycasts = false;
     }
 
@@ -45,6 +45,7 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IPointerDownHandler, IBe
     {
         //Move like cursor
         rectTransform.anchoredPosition += eventData.delta;
+        rectTransform.localScale = new Vector3(2f, 2f, 1f);
     }
 
     //Unclick
@@ -52,6 +53,7 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IPointerDownHandler, IBe
     {
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
+        rectTransform.localScale = new Vector3(1f, 1f, 1f);
         rectTransform.localPosition = posInicial;
     }
 
@@ -59,6 +61,7 @@ public class DragAndDrop : MonoBehaviour, IDragHandler, IPointerDownHandler, IBe
     {
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
+        rectTransform.localScale = new Vector3(1f, 1f, 1f);
         rectTransform.localPosition = posInicial;
     }
 }
