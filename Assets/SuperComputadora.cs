1907c1a833a0cc67691f2069403571bf9677728e
Jose Varela <varelacen@hotmail.com>
2020-11-21 22:52:31 -0300

    Supercomputadora Online dijo el michael jackson

---
+++ Assets/SuperComputadora.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperComputadora : MonoBehaviour
+{
+    [SerializeField] bool team;
+    public float timer;
+    public int moneyToGive;
+
+    private void Start()
+    {
+        if (team) 
+        {
+            StartCoroutine(GiveMoneyA());
+        }
+        else 
+        {
+            StartCoroutine(GiveMoneyB());
+        }
+    }
+
+    IEnumerator GiveMoneyA()
+    {
+        yield return new WaitForSeconds(timer);
+        Minijuegos.m_clicksA(moneyToGive);
+        StartCoroutine(GiveMoneyA());
+        yield break;
+    }
+    IEnumerator GiveMoneyB()
+    {
+        yield return new WaitForSeconds(timer);
+        Minijuegos.m_clicksB(moneyToGive);
+        StartCoroutine(GiveMoneyB());
+        yield break;
+    }
+}
