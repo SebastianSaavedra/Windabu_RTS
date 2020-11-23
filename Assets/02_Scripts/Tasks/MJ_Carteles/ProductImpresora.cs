e05b3c34d8973f3beb718f827345b4608f901b0d
Carlos Michea A <carlos.15.michea@gmail.com>
2020-11-23 02:12:04 -0300

    Minijuegos de produccion

---
+++ Assets/02_Scripts/Tasks/MJ_Carteles/ProductImpresora.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProductImpresora : MonoBehaviour
+{
+    public GameObject text;
+    private int partesCounter;
+    public int maxPartes;
+
+    private void Start()
+    {
+        text.SetActive(false);
+    }
+
+    public void AddCounter()
+    {
+        // Complete MJ
+        partesCounter++;
+        if (partesCounter == maxPartes)
+        {
+            StartCoroutine(WaitToFinish());
+        }
+    }
+    IEnumerator WaitToFinish()
+    {
+        text.SetActive(true);
+        yield return new WaitForSeconds(15);
+
+        text.SetActive(false);
+        GetComponentInParent<FinishMinigameManager>().AddSlotCounter();
+        Debug.Log("Carga2");
+    }
+}
