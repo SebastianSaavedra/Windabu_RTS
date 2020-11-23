732a5365f1bfdb0f961c29bb8e3cda17af7c8e97
Sebastian_Saavedra <sebastiansaavedr@gmail.com>
2020-11-22 22:07:31 -0300

    Sabotaje (?

---
+++ Assets/02_Scripts/RepararSabotaje.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class RepararSabotaje : MonoBehaviourPunCallbacks
+{
+    [SerializeField] List<BoxCollider2D> boxCollider2Ds = new List<BoxCollider2D>();
+    bool enBase;
+    BoxCollider2D colliderDesactivado;
+
+    private void Start()
+    {
+        foreach (GameObject col in GameObject.FindGameObjectsWithTag("Actividad B"))
+        {
+            boxCollider2Ds.Add(col.GetComponent<BoxCollider2D>());
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Base") && !enBase)
+        {
+            enBase = true;
+        }
+
+        if (enBase)
+        {
+            Debug.Log("Esta en la base");
+            photonView.RPC("BuscarCollider",RpcTarget.MasterClient);
+        }
+    }
+
+    [PunRPC]
+    void BuscarCollider()
+    {
+        foreach (BoxCollider2D item in boxCollider2Ds)
+        {
+            if (item.enabled == false)
+            {
+                Debug.Log("El collider desactivado es: " + item);
+                colliderDesactivado = item;
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    if (PhotonNetwork.IsMasterClient)
+                    {
+                        photonView.RPC("LlamarCorutinaReparar", RpcTarget.AllViaServer);
+                    }
+                }
+            }
+        }
+    }
+
+    [PunRPC]        //Llamarlo desde otros scripts
+    public void ActualizarListaDeColliders(BoxCollider2D col)
+    {
+        boxCollider2Ds.Add(col);
+    }
+
+    [PunRPC]
+    void LlamarCorutinaReparar()
+    {
+        StartCoroutine("Reparar");
+    }
+
+
+    IEnumerator Reparar()
+    {
+        Debug.Log("Empezo a reparar");
+        yield return new WaitForSeconds(5f);
+        colliderDesactivado.enabled = true;
+        Debug.Log("Ya Reparo");
+        yield break;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        enBase = false;
+        Debug.Log("El estado del bool de la base es: " + enBase);
+    }
+}
---
+++ Assets/02_Scripts/Sabotage.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Sabotage : MonoBehaviourPunCallbacks
+{
+    [SerializeField] Com.MaluCompany.TestGame.TEST_Movement movement;
+    [SerializeField] PlayerTeam playerTeam;
+    //Coroutine cor;
+    public List<Player> playersActuales = new List<Player>();
+
+    [SerializeField] BoxCollider2D colliderAct;
+
+    //private new void OnEnable()
+    //{
+    //    playerTeam.GetComponent<PlayerTeam>();
+    //    Debug.Log(playerTeam);
+    //}
+
+    private void Awake()
+    {
+        //movement.GetComponent<Com.MaluCompany.TestGame.TEST_Movement>();
+        if (PhotonNetwork.IsMasterClient)   //Tener en mente que este codigo puede producir errores a futuro.
+        {
+            //SetearUIMasterClient();
+            playersActuales.Add(PhotonNetwork.LocalPlayer);
+            //Debug.Log("On joined room ha sido debugea2 (?");
+        }
+    }
+
+    Player TargetPlayerByActorNumber(int playerActorNumber)
+    {
+        //Solamente se tiene para mostrar el número de jugadores y su actorNumber
+        foreach (var item in playersActuales)
+        {
+            Debug.Log("Players: " + item.ActorNumber);
+        }
+
+        //Determina a qué jugador debemos enviarle el RPC según su actor number 
+        //(identificador de cada jugador en esta sala de juego)
+        Player playerToReturn = null;
+        for (int i = 0; i < playersActuales.Count; i++)
+        {
+            if (playersActuales[i].ActorNumber == playerActorNumber)
+            {
+                playerToReturn = playersActuales[i];
+                break;
+            }
+        }
+        return playerToReturn;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Entro al collider de la wea");
+        if (this.playerTeam.TeamA && collision.CompareTag("Actividad B"))
+        {
+            Debug.Log("El man es del Team A");
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("Presiono la F y es del team A");
+                photonView.RPC("LlamarCorutinaSabotaje", RpcTarget.AllViaServer);
+            }
+        }
+
+        if (this.playerTeam.TeamB && collision.CompareTag("Actividad A"))
+        {
+            Debug.Log("El man es del Team B");
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Debug.Log("Presiono la F y es del team B");
+                photonView.RPC("LlamarCorutinaSabotaje", RpcTarget.AllViaServer);
+            }
+        }
+    }
+
+    [PunRPC]
+    IEnumerator Sabotaje()
+    {
+        Debug.Log("Llego a la corutina del sab");
+        movement.enabled = false;
+        yield return new WaitForSeconds(15f);
+        colliderAct.enabled = false;
+        movement.enabled = true;
+        if (colliderAct != null)
+        {
+            colliderAct = null;
+            Debug.Log("Perdio el colliderAct");
+        }
+        yield break;
+    }
+
+    [PunRPC]
+    void LlamarCorutinaSabotaje()
+    {
+        StartCoroutine("Sabotaje");
+    }
+    //[PunRPC]
+    //public void InterrumpirSabotaje()
+    //{
+    //    StopCoroutine("Sabotaje");
+    //    movement.enabled = true;
+    //    colliderAct = null;
+    //    Debug.Log("Me han detenido el sabotaje chavales o.0!");
+    //}
+
+    //private void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    if (playerTeam.TeamA && collision.gameObject.GetComponent<PlayerTeam>().TeamB)
+    //    {
+    //        Debug.Log("Toco al player enemigo");
+    //        if (Input.GetKeyDown(KeyCode.R))
+    //        {
+    //            photonView.RPC("InterrumpirSabotaje", TargetPlayerByActorNumber(collision.gameObject.GetComponent<PlayerId>().id));
+    //            Debug.Log("El Id del player que toco es: " + collision.gameObject.GetComponent<PlayerId>().id);
+    //            //collision.GetComponent<Sabotage>().InterrumpirSabotaje();
+    //        }
+    //    }
+
+    //    if (playerTeam.TeamB && collision.gameObject.GetComponent<PlayerTeam>().TeamA)
+    //    {
+    //        Debug.Log("Toco al player enemigo");
+    //        if (Input.GetKeyDown(KeyCode.R))
+    //        {
+    //            photonView.RPC("InterrumpirSabotaje", TargetPlayerByActorNumber(collision.gameObject.GetComponent<PlayerId>().id));
+    //            Debug.Log("El Id del player que toco es: " + collision.gameObject.GetComponent<PlayerId>().id);
+    //            //collision.GetComponent<Sabotage>().InterrumpirSabotaje();
+    //        }
+    //    }
+
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (this.playerTeam.TeamA && collision.CompareTag("Actividad B"))
+        {
+            colliderAct = collision.GetComponent<BoxCollider2D>();
+            Debug.Log("Agarro el colliderAct: " + colliderAct);
+        }
+
+        if (this.playerTeam.TeamB && collision.CompareTag("Actividad A"))
+        {
+            colliderAct = collision.GetComponent<BoxCollider2D>();
+            Debug.Log("Agarro el colliderAct: " + colliderAct);
+        }
+    }
+
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (colliderAct != null)
+    //    {
+    //        colliderAct = null;
+    //        Debug.Log("Perdio el colliderAct");
+    //    }
+    //}
+}
