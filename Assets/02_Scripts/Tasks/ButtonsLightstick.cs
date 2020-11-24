123be0bccf8d56f5da08ca8e8a90f2fc7f9f739e
Carlos Michea A <carlos.15.michea@gmail.com>
2020-11-24 08:32:55 -0300

    MJ produccion de impresora3d

---
+++ Assets/02_Scripts/Tasks/ButtonsLightstick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsLightstick : MonoBehaviour
+{
+    int counter;
+
+    private void Start()
+    {
+        counter = 0;
+    }
+
+    public void Pressed()
+    {
+        counter++;
+        if(counter == 5)
+        {
+            GetComponentInParent<ProductImpresora>().AddCounter();
+        }
+    }
+}
