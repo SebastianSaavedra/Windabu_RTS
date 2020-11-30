52e8493bd989c4ed814441146b4ecf66e0cf148a
Jose Varela <varelacen@hotmail.com>
2020-11-30 17:31:50 -0300

    Mas cambios
    bruh

--- Assets/02_Scripts/Tasks/ShopManager.cs
+++ Assets/02_Scripts/Tasks/ShopManager.cs
@@ -14,7 +14,7 @@ public class ShopManager : MonoBehaviourPunCallbacks
     float impresoraCost = 100f, impresoraDelay = 15f, imp3dCost = 200f, imp3dDelay = 30f, chapitaCost = 150f, chapitaDelay = 20f;
     public bool canBuy_0, canBuy_1, canBuy_2;
     //Prefabs
-    public GameObject impresora, imp3d, chapita;
+    public GameObject impresora, imp3d, chapita,impresoraInd,merch,plotter,superComputadora_1, superComputadora_2, superComputadora_3;
     [SerializeField] bool team;
     [SerializeField] UIManager uiMan;
 
@@ -25,31 +25,47 @@ public class ShopManager : MonoBehaviourPunCallbacks
 
     public void OtraCompra()
     {
-        if (team) 
+        if (team)
         {
-        if(MinigameManager.dineroA >= impresoraCost)// volver a referencia teamManager
-        {
-            StartCoroutine(SpawnItem(impresoraDelay, 0));
-            Minijuegos.compraA((int)impresoraCost);
-            uiMan.CallRpc(team,1);
-            //gameObject.GetComponent<TeamManager>().money -= impresoraCost;
+            if (MinigameManager.dineroA >= impresoraCost)// volver a referencia teamManager
+            {
+                StartCoroutine(SpawnItem(impresoraDelay, 0));//ActivateItem
+                Minijuegos.compraA((int)impresoraCost);
+                uiMan.CallRpc(team, 1);// Para Queue Line
+                                       //gameObject.GetComponent<TeamManager>().money -= impresoraCost;
+            }
+            else
+            {
+                Debug.Log("Dinero Insuficiente Pe");
+            }
         }
-        else
+    }
+    [PunRPC]
+    public void cantBuy(int wichItem) 
+    {
+        switch (wichItem) 
         {
-            Debug.Log("Dinero Insuficiente Pe");
-        }
+            case 0:
+                canBuy_0 = false;
+                break;
+            case 1:
+                canBuy_1 = false;
+                break;
+            case 2:
+                canBuy_2 = false;
+                break;
         }
     }
     public void Impresora()
     {
         if (team)
         {
-            if (MinigameManager.dineroA >= impresoraCost)// volver a referencia teamManager
+            if (MinigameManager.dineroA >= impresoraCost && canBuy_0)// volver a referencia teamManager
             {
                 RPCcor(impresoraDelay,0);
                 Minijuegos.compraA((int)impresoraCost);
                 uiMan.CallRpc(team, 0);
-                canBuy_0 = true;
+                photonView.RPC("cantBuy", RpcTarget.AllViaServer, 0);
                 //gameObject.GetComponent<TeamManager>().money -= impresoraCost;
             }
             else
@@ -59,12 +75,12 @@ public class ShopManager : MonoBehaviourPunCallbacks
         }
         else
         {
-            if (MinigameManager.dineroB >= impresoraCost)// volver a referencia teamManager
+            if (MinigameManager.dineroB >= impresoraCost && canBuy_0)// volver a referencia teamManager
             {
                 RPCcor(impresoraDelay,0);
                 MinigameManager.dineroB -= (int)impresoraCost;
                 uiMan.CallRpc(team, 0);
-                canBuy_0 = true;
+                photonView.RPC("cantBuy", RpcTarget.AllViaServer, 0);
                 //gameObject.GetComponent<TeamManager>().money -= impresoraCost;
             }
             else
@@ -77,12 +93,12 @@ public class ShopManager : MonoBehaviourPunCallbacks
     {
         if (team)
         {
-            if (MinigameManager.dineroA >= (int)imp3dCost)// volver a referencia teamManager
+            if (MinigameManager.dineroA >= (int)imp3dCost && canBuy_1)// volver a referencia teamManager
             {
                 RPCcor(imp3dDelay, 1);
                 Minijuegos.compraA((int)imp3dCost);
                 uiMan.CallRpc(team, 1);
-                canBuy_0 = true;
+                photonView.RPC("cantBuy", RpcTarget.AllViaServer, 1);
                 //gameObject.GetComponent<TeamManager>().money -= impresoraCost;
             }
             else
@@ -92,12 +108,12 @@ public class ShopManager : MonoBehaviourPunCallbacks
         }
         else
         {
-            if (MinigameManager.dineroB >= (int)imp3dCost)// volver a referencia teamManager
+            if (MinigameManager.dineroB >= (int)imp3dCost && canBuy_1)// volver a referencia teamManager
             {
                 RPCcor(imp3dDelay, 1);
                 MinigameManager.dineroB -= (int)imp3dCost;
                 uiMan.CallRpc(team, 1);
-                canBuy_0 = true;
+                photonView.RPC("cantBuy", RpcTarget.AllViaServer, 1);
                 //gameObject.GetComponent<TeamManager>().money -= impresoraCost;
             }
             else
@@ -110,12 +126,12 @@ public class ShopManager : MonoBehaviourPunCallbacks
     {
         if (team)
         {
-            if (MinigameManager.dineroA >= (int)chapitaCost)// volver a referencia teamManager
+            if (MinigameManager.dineroA >= (int)chapitaCost && canBuy_2)// volver a referencia teamManager
             {
                 RPCcor(chapitaDelay, 2);
                 Minijuegos.compraA((int)chapitaCost);
                 uiMan.CallRpc(team, 2);
-                canBuy_0 = true;
+                photonView.RPC("cantBuy", RpcTarget.AllViaServer,2);
                 //gameObject.GetComponent<TeamManager>().money -= impresoraCost;
             }
             else
@@ -125,12 +141,12 @@ public class ShopManager : MonoBehaviourPunCallbacks
         }
         else
         {
-            if (MinigameManager.dineroB >= (int)chapitaCost)// volver a referencia teamManager
+            if (MinigameManager.dineroB >= (int)chapitaCost && canBuy_2)// volver a referencia teamManager
             {
                 RPCcor(chapitaDelay, 2);
                 MinigameManager.dineroB -= (int)chapitaCost;
                 uiMan.CallRpc(team, 2);
-                canBuy_0 = true;
+                photonView.RPC("cantBuy", RpcTarget.AllViaServer,2);
                 //gameObject.GetComponent<TeamManager>().money -= impresoraCost;
             }
             else
@@ -143,8 +159,8 @@ public class ShopManager : MonoBehaviourPunCallbacks
 
     public void RPCcor(float time, int whatItem) 
     {
-        // photonView.RPC("StartCor", RpcTarget.MasterClient,time,whatItem);
-        StartCor(time, whatItem);
+         photonView.RPC("StartCor", RpcTarget.AllViaServer,time,whatItem);
+        //StartCor(time, whatItem);
     }
 
     [PunRPC]
@@ -160,22 +176,25 @@ public class ShopManager : MonoBehaviourPunCallbacks
         switch (itemToSpawn) 
         {
             case 0:
-            GameObject spawned = PhotonNetwork.Instantiate(impresora.name, itemSpawnerPos.position, Quaternion.identity);
-            spawned.transform.parent = itemSpawnerPos.transform;
-            spawned.GetComponentInChildren<TaskDropDownMinigame>().taskBarPanel = itemSpawnerPos.GetComponent<DataSaver>().originalPanel;
-                spawned.GetComponentInChildren<TaskDropDownMinigame>().managerMinijuegos = itemSpawnerPos.GetComponent<DataSaver>().manager;
+                //GameObject spawned = PhotonNetwork.Instantiate(impresora.name, itemSpawnerPos.position, Quaternion.identity);
+                //spawned.transform.parent = itemSpawnerPos.transform;
+                //spawned.GetComponentInChildren<TaskDropDownMinigame>().taskBarPanel = itemSpawnerPos.GetComponent<DataSaver>().originalPanel;
+                //    spawned.GetComponentInChildren<TaskDropDownMinigame>().managerMinijuegos = itemSpawnerPos.GetComponent<DataSaver>().manager;
+                impresora.SetActive(true);
                 break;
             case 1:
-                GameObject spawned2 = PhotonNetwork.Instantiate(imp3d.name, itemSpawnerPos.position, Quaternion.identity);
-                spawned2.transform.parent = itemSpawnerPos.transform;
-                spawned2.GetComponentInChildren<TaskDropDownMinigame>().taskBarPanel = itemSpawnerPos.GetComponent<DataSaver>().originalPanel;
-                spawned2.GetComponentInChildren<TaskDropDownMinigame>().managerMinijuegos = itemSpawnerPos.GetComponent<DataSaver>().manager;
+                //GameObject spawned2 = PhotonNetwork.Instantiate(imp3d.name, itemSpawnerPos.position, Quaternion.identity);
+                //spawned2.transform.parent = itemSpawnerPos.transform;
+                //spawned2.GetComponentInChildren<TaskDropDownMinigame>().taskBarPanel = itemSpawnerPos.GetComponent<DataSaver>().originalPanel;
+                //spawned2.GetComponentInChildren<TaskDropDownMinigame>().managerMinijuegos = itemSpawnerPos.GetComponent<DataSaver>().manager;
+                imp3d.SetActive(true);
                 break;
             case 2:
-                GameObject spawned3 = PhotonNetwork.Instantiate(chapita.name, itemSpawnerPos.position, Quaternion.identity);
-                spawned3.transform.parent = itemSpawnerPos.transform;
-                spawned3.GetComponentInChildren<TaskDropDownMinigame>().taskBarPanel = itemSpawnerPos.GetComponent<DataSaver>().originalPanel;
-                spawned3.GetComponentInChildren<TaskDropDownMinigame>().managerMinijuegos = itemSpawnerPos.GetComponent<DataSaver>().manager;
+                //GameObject spawned3 = PhotonNetwork.Instantiate(chapita.name, itemSpawnerPos.position, Quaternion.identity);
+                //spawned3.transform.parent = itemSpawnerPos.transform;
+                //spawned3.GetComponentInChildren<TaskDropDownMinigame>().taskBarPanel = itemSpawnerPos.GetComponent<DataSaver>().originalPanel;
+                //spawned3.GetComponentInChildren<TaskDropDownMinigame>().managerMinijuegos = itemSpawnerPos.GetComponent<DataSaver>().manager;
+                chapita.SetActive(true);
                 break;
         }
         
