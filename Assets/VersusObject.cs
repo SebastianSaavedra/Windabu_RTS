ea6ae37544d50ccdcb83932889d5ad51b2e7949d
Sebastian_Saavedra <sebastiansaavedr@gmail.com>
2020-12-03 10:25:53 -0300

    VS wea

---
+++ Assets/VersusObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VersusObject : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        StartCoroutine(Versus());
+    }
+
+    IEnumerator Versus()
+    {
+        yield return new WaitForSeconds(2f);
+        gameObject.SetActive(false);
+        yield break;
+    }
+}
